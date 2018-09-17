a73886df49abb0fd82d4bea487bac9147ab22f93
Jaime <jaimesof82@gmail.com>
2018-09-17 14:58:20 -0700

    More Practice. No changes.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 //FUNCTION REVERSES A STRING with built in functions
 
-// function reverseString(input) {
-//   let splitString = input.split("");
-//   let reverseArray = splitString.reverse();
-//   let glueArray = reverseArray.join("");
-//   return glueArray;
-// }
+function reverseString(input) {
+  let splitString = input.split("");
+  let reverseArray = splitString.reverse();
+  let glueArray = reverseArray.join("");
+  return glueArray;
+}
 
-// console.log(reverseString("syawla...uoy htiw eb lliw ecroF ehT"));
+console.log(reverseString("syawla...uoy htiw eb lliw ecroF ehT"));
 
-// //SHORTER VERSION OF THE REVERSE STRING FUNCTION ABOVE
+//SHORTER VERSION OF THE REVERSE STRING FUNCTION ABOVE
 
-// function backwardString(input) {
-//   return input
-//     .split("")
-//     .reverse()
-//     .join("");
-// }
+function backwardString(input) {
+  return input
+    .split("")
+    .reverse()
+    .join("");
+}
 
-// console.log(backwardString("rettam edurc siht ton ,era ew sgieB suonimuL"));
+console.log(backwardString("rettam edurc siht ton ,era ew sgieB suonimuL"));
 
-// //FUNCTION THAT REVERSES A STRING WITH A FOR LOOP
+//FUNCTION THAT REVERSES A STRING WITH A FOR LOOP
 
-// function reverseLoop(input) {
-//   let moonwalkString = "";
+function reverseLoop(input) {
+  let moonwalkString = "";
 
-//   for (let i = input.length - 1; i >= 0; i--) {
-//     moonwalkString += input[i];
-//   }
-//   return moonwalkString;
-// }
+  for (let i = input.length - 1; i >= 0; i--) {
+    moonwalkString += input[i];
+  }
+  return moonwalkString;
+}
 
-// console.log(
-//   reverseLoop("uoy lortnoc lliw ecroF ruoy ro ,ecroF rouy lortnoc ot nraeL")
-// );
+console.log(
+  reverseLoop("uoy lortnoc lliw ecroF ruoy ro ,ecroF rouy lortnoc ot nraeL")
+);
