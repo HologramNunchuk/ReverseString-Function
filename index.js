23dfb641b8f9ef1bdfa56b3c78baeced751629cd
Jaime <jaimesof82@gmail.com>
2018-09-17 17:16:16 -0700

    tidy up the code

--- index.js
+++ index.js
@@ -35,16 +35,16 @@ console.log(
   reverseLoop("uoy lortnoc lliw ecroF ruoy ro ,ecroF rouy lortnoc ot nraeL")
 );
 
-//FUNCTION THAT CAPITALIZES THE FIRST LETTER OF EVERY WORD BY SEPARATING THE FIRST LETTER FROM THE REST OF THE WORD
+FUNCTION THAT CAPITALIZES THE FIRST LETTER OF EVERY WORD BY SEPARATING THE FIRST LETTER FROM THE REST OF THE WORD
 
-// function letterCapitalize(str) {
-//   let words = str.split(" ");
+function letterCapitalize(str) {
+  let words = str.split(" ");
 
-//   for (let i = 0; i < words.length; i++) {
-//     words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
-//   }
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+  }
 
-//   return words.join(" ");
-// }
+  return words.join(" ");
+}
 
-// console.log(letterCapitalize("hello from tokyo!!"));
+console.log(letterCapitalize("hello from tokyo!!"));
