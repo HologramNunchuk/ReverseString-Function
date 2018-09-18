37c6df65fc0c75373496f1a49e21086f0a82d038
Jaime <jaimesof82@gmail.com>
2018-09-17 17:00:15 -0700

    no change

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //FUNCTION REVERSES A STRING with built in functions
 
-function reverseString(input) {
-  let splitString = input.split("");
-  let reverseArray = splitString.reverse();
-  let glueArray = reverseArray.join("");
-  return glueArray;
-}
+// function reverseString(input) {
+//   let splitString = input.split("");
+//   let reverseArray = splitString.reverse();
+//   let glueArray = reverseArray.join("");
+//   return glueArray;
+// }
 
-console.log(reverseString("syawla...uoy htiw eb lliw ecroF ehT"));
+// console.log(reverseString("syawla...uoy htiw eb lliw ecroF ehT"));
 
 //SHORTER VERSION OF THE REVERSE STRING FUNCTION ABOVE
 
@@ -22,29 +22,29 @@ console.log(backwardString("rettam edurc siht ton ,era ew sgieB suonimuL"));
 
 //FUNCTION THAT REVERSES A STRING WITH A FOR LOOP
 
-function reverseLoop(input) {
-  let moonwalkString = "";
+// function reverseLoop(input) {
+//   let moonwalkString = "";
 
-  for (let i = input.length - 1; i >= 0; i--) {
-    moonwalkString += input[i];
-  }
-  return moonwalkString;
-}
+//   for (let i = input.length - 1; i >= 0; i--) {
+//     moonwalkString += input[i];
+//   }
+//   return moonwalkString;
+// }
 
-console.log(
-  reverseLoop("uoy lortnoc lliw ecroF ruoy ro ,ecroF rouy lortnoc ot nraeL")
-);
+// console.log(
+//   reverseLoop("uoy lortnoc lliw ecroF ruoy ro ,ecroF rouy lortnoc ot nraeL")
+// );
 
 //FUNCTION THAT CAPITALIZES THE FIRST LETTER OF EVERY WORD BY SEPARATING THE FIRST LETTER FROM THE REST OF THE WORD
 
-function letterCapitalize(str) {
-  let words = str.split(" ");
+// function letterCapitalize(str) {
+//   let words = str.split(" ");
 
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
-  }
+//   for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+//   }
 
-  return words.join(" ");
-}
+//   return words.join(" ");
+// }
 
-console.log(letterCapitalize("hello from tokyo!!"));
+// console.log(letterCapitalize("hello from tokyo!!"));
